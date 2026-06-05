aaab4571549830c9369039e1ab29749228bea100
GustavoLuzz <gustavo_lux@outlook.com>
2026-06-05 14:25:53 -0300

    🔧 config: Configure cors allowed origins, allowing local connection

--- src/main/java/com/gustavoluz/spendwise_api/config/security/CorsConfig.java
+++ src/main/java/com/gustavoluz/spendwise_api/config/security/CorsConfig.java
@@ -9,7 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://192.168.100.62:3000"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
